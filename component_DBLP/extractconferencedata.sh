233e1befee1da0040ec57f4122d338c0a294f881
YaoPeng <secoder@DebYP>
2017-07-26 14:35:29 +0300

    update the extractconferencedata.sh to get booktitle also

--- component_DBLP/extractconferencedata.sh
+++ component_DBLP/extractconferencedata.sh
@@ -3,21 +3,34 @@
 
 function extractlines {
 	#echo $3
-	sed -n -e "$1 , $2 p" ../title.txt >> "${3}"_titles.txt
-	sed -n -e "$1 , $2 p" ../authors.txt >> "${3}"_authors.txt
-	sed -n -e "$1 , $2 p" ../year.txt >> "${3}"_years.txt
+	sed -n -e "$1 , $2 p" ../title_author_conf_year/title.txt >> "${3}"_titles.txt
+	sed -n -e "$1 , $2 p" ../title_author_conf_year/authors.txt >> "${3}"_authors.txt
+	sed -n -e "$1 , $2 p" ../title_author_conf_year/year.txt >> "${3}"_years.txt
+	sed -n -e "$1 , $2 p" ../title_author_conf_year/booktitle.txt >> "${3}"_booktitle.txt
 	#echo "new line" >> ${3}_year.txt
 }
 
 
 
 if [[ -n "$1" ]]; then
-	idx=`grep -nw $1 ../booktitle.txt | cut -f1 -d:` 
+        printf "Here are the conferences contain $1\n"
+        grep -w $1 ../title_author_conf_year/booktitle.txt | sort | uniq
+        printf "Continue [Y/y]"
+
+        read input
+
+        if [ "$input" != "Y" ] && [ "$input" != "y" ]; then
+		printf "$input Exit\n"
+                exit
+	fi
+
+	idx=`grep -nw $1 ../title_author_conf_year/booktitle.txt | cut -f1 -d:` 
 else
     	printf "Usage: $0 Conference Name\nexample: $0 HICSS\n"
 	exit
 fi
 
+printf "start processing....\n"
 
 c=1
 s=0

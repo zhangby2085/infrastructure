135fa9ca3d361c3856f1a6d401d4ed51c78a2d61
zaza <zaza4504@163.com>
2017-07-31 13:14:34 +0300

    update the extraconferencedata.sh to put all the preprocess in one scrip

--- component_DBLP/example.py
+++ component_DBLP/example.py
@@ -5,46 +5,22 @@ Created on Thu Jun 15 14:17:11 2017
 @author: secoder
 """
 
-
-import io
-import json
-
 from recommendationsys import recommendationsys
 
+user1 = 'Mikko Valkama'
+user2 = 'Aino Ahtinen'
+user3 = 'Heli Väätäjä'
+user4 = 'Harri Siirtola'
+user5 = 'Yevgeni Koucheryavy'
 
-# initiate the recommendation system for CHI
-# 10 is the cluster number(useless in current version, just ignor)
-# 3 is the threshold for the number of the publications of a recommendation
-# 2013 is the threshold for the most recent year of the publication of a recommendation
-remdCHI = recommendationsys('./CHI/CHI_titles.txt','./CHI/CHI_authors.txt',
-                            './CHI/CHI_years.txt', './CHI/CHI_booktitle.txt', 10, 3, 2013)
+remd = recommendationsys('./Yevgeni/titles.txt','./Yevgeni/authors.txt',
+                            './Yevgeni/years.txt', './Yevgeni/booktitles.txt', 10, 3, 2013)
 
-remd = recommendationsys('./HICSS_titles.txt','./HICSS_authors.txt',
-                             './HICSS_years.txt','./HICSS_booktitle.txt',10, 3, 2013)
+recommendauthors = remd.recommendationV3(user5,3)
 
-remdKATE = recommendationsys('./KATE/WWIC_ICC_GLOBECOM_titles.txt','./KATE/WWIC_ICC_GLOBECOM_authors.txt',
-                             './KATE/WWIC_ICC_GLOBECOM_years.txt','./KATE/WWIC_ICC_GLOBECOM_booktitle.txt',10, 3, 2013)
+remd.save_json(user5)
 
-
-# users
-user1 = 'aaa'
-user2 = 'bbb'
-user3 = 'ccc'
-
-user4 = 'ddd 001'
-user5 = 'eee'
-
-# by default the system will generate 3 groups recommendations
-# example: remd.recommendationV3(user2,3) will return 9 recommendations 
-# for user2 and 3 in each group 
-
-recommendauthors2 = remd.recommendationV3(user2,3)
-recommendauthors3 = remdCHI.recommendationV3(user3,3)
-recommendauthors4 = remdKATE.recommendationV3(user4,3)
-
-# print the recommendations and their relevcancy (distance) for user2
-for author in recommendauthors2['recommendations']:
+# print the recommendations and relevancy
+for author in recommendauthors['recommendations']:
     print author['name'] + ' ' + str(author['relevancy'])
-
-# save the recommendations to json file aaa.json
-remd.save_json('aaa')
+    
--- component_DBLP/extractconferencedata.sh
+++ component_DBLP/extractconferencedata.sh
@@ -1,57 +1,106 @@
 #!/bin/bash
-#idx=(2 3 4 6 7 9 12 13)
 
 function extractlines {
 	#echo $3
-	sed -n -e "$1 , $2 p" ../title_author_conf_year/title.txt >> "${3}"_titles.txt
-	sed -n -e "$1 , $2 p" ../title_author_conf_year/authors.txt >> "${3}"_authors.txt
-	sed -n -e "$1 , $2 p" ../title_author_conf_year/year.txt >> "${3}"_years.txt
-	sed -n -e "$1 , $2 p" ../title_author_conf_year/booktitle.txt >> "${3}"_booktitle.txt
+	sed -n -e "$1 , $2 p" ./title_author_conf_year/title.txt >> ./"$4"/"${3}"_titles.txt
+	sed -n -e "$1 , $2 p" ./title_author_conf_year/authors.txt >> ./"$4"/"${3}"_authors.txt
+	sed -n -e "$1 , $2 p" ./title_author_conf_year/year.txt >> ./"$4"/"${3}"_years.txt
+	sed -n -e "$1 , $2 p" ./title_author_conf_year/booktitleLF.txt >> ./"$4"/"${3}"_booktitle.txt
 	#echo "new line" >> ${3}_year.txt
 }
 
+# check the input args and print the usage
+if [ "$#" -lt 2 ]; then
+        echo "Usage: ./DBLPRecSys name conference1 conference2 conference3 ... "
+        echo "Example: ./DBLPRecSys \"Mike Jordan\" \"CHI\" \"HICSS\" "
+	exit
+fi
 
+echo "System init ...."
 
-if [[ -n "$1" ]]; then
-        printf "Here are the conferences contain $1\n"
-        grep -w $1 ../title_author_conf_year/booktitle.txt | sort | uniq
-        printf "Continue [Y/y]"
+# check if all the files are there
+if [ ! -f ./DBLPdataset/dblp.dtd ] && [ ! -f ./DBLPdataset/dblp.xml ]; then
+        printf "File dblp.xml, dblp.dtd not found, please check ./DBLPdataset directory\n"
+        exit
+fi
 
-        read input
+# check if the titles, authors, years, booktitiles are ready
 
-        if [ "$input" != "Y" ] && [ "$input" != "y" ]; then
-		printf "$input Exit\n"
-                exit
+if [ ! -d ./title_author_conf_year ]; then
+        printf "Create direcotry ./title_author_conf_year\n"
+        mkdir title_author_conf_year
+        python ./getConf.py
+        python ./getTitle.py
+        python ./getAuthor.py
+        python ./getYear.py
+        tr -d '\r' < booktitle.txt >  booktitleLF.txt
+        rm booktitle.txt
+        mv *txt title_author_conf_year
+fi
+
+# ------------------------------------
+# start extract target conference data
+# -----------------------------------
+
+if [ ! -f ./title_author_conf_year/booktitleLF.txt ]; then
+	printf "File ./title_author_conf_year/booktitleLF.txt not found\n"
+	exit
+fi
+                
+printf "Start extract conference data:\n\n"
+
+l=0
+for x in "$@"; do
+	# the first arg is the user name, make a dir for him/her
+	if [ $l == 0 ]; then
+		username=$x
+		mkdir "$username"
+		l=1
+		continue
 	fi
 
-	idx=`grep -nw $1 ../title_author_conf_year/booktitle.txt | cut -f1 -d:` 
-else
-    	printf "Usage: $0 Conference Name\nexample: $0 HICSS\n"
+	echo "Processing $x ..."
+	#result=`grep -n ^"$x"$ ./title_author_conf_year/booktitleLF.txt | cut -f1 -d:` 
+	result=`grep -nw "$x" ./title_author_conf_year/booktitleLF.txt | cut -f1 -d:` 
+        if [ -z "$result" ]; then
+                printf "No conference found: $x\n"
+                continue
+        fi
+
+	echo "Done for $x"
+
+	if [ $l == 1 ]; then
+		#printf "1st conference\n"
+		idx=$result
+		l=2
+	else
+		idx="$idx"$'\n'"$result"
+	fi
+done
+
+echo "$idx" > ./"$username"/idx2
+        #result=`grep -w $1 ./booktitleLF.txt | sort | uniq`
+        #printf "$idx\n"
+if [ -z "$idx" ]; then
+	printf "No related conference found, please check the conference name and try it again\n"
 	exit
+fi
+
+printf "Continue [Y/y]"
+
+read input
+
+if [ "$input" != "Y" ] && [ "$input" != "y" ]; then
+	printf "$input Exit\n"
+        exit
 fi
 
 printf "start processing....\n"
 
-c=1
-s=0
-f=0
+awk 'FNR==NR{line[$0]=$0; next} FNR in line' ./"$username"/idx2 ./title_author_conf_year/title.txt >> ./"$username"/titles.txt
+awk 'FNR==NR{line[$0]=$0; next} FNR in line' ./"$username"/idx2 ./title_author_conf_year/authors.txt >> ./"$username"/authors.txt
+awk 'FNR==NR{line[$0]=$0; next} FNR in line' ./"$username"/idx2 ./title_author_conf_year/year.txt >> ./"$username"/years.txt
+awk 'FNR==NR{line[$0]=$0; next} FNR in line' ./"$username"/idx2 ./title_author_conf_year/booktitleLF.txt >> ./"$username"/booktitles.txt
 
-for i in $idx; do
-	if [ $s -eq 0 ]; then
-		s=$i
-		continue
-	fi
-
-	if [ $(($i-$s)) -eq $c ]; then 
-		c=$(($c+1))	
-	else
-		f=$(($c+$s-1))
-		#echo $s $f 
-		extractlines $s $f $1
-		s=$i
-		c=1
-	fi
-done
-
-#echo $s $i
-extractlines $s $i $1
+# remove the " in the author.txt
+sed -i -e 's/"//g' ./"$username"/authors.txt

afaa80650ad9419bbf4633ed068d658bb55f1c50
Cloud User <cloud-user@cobweb.novalocal>
2017-10-30 00:10:21 +0000

    collect tweets and save into mongoDB, support re-collection from the broken point

--- component_twitter_networks/config.py
+++ component_twitter_networks/config.py
@@ -1,4 +1,31 @@
-id_or_screen_name = 'aaa bbb'
-project_name = 'test'
-db_name = 'test1'
-collection_name = 'commontweets'
+# user id or screen name of the target user 
+# from who the follower and their tweets will be collected
+#id_or_screen_name = 'winterdamsel Roc_Yao zhangby2085'
+id_or_screen_name = 'TampereUniTech TAMK_UAS UniTampere Tampere_3'
+
+# project name
+project_name = 'Tampere3'
+
+# mongoDB host
+db_host = 'localhost'
+
+# mongoDB port
+db_port = 27017
+
+# database name 
+db_name = 'tampere3'
+
+# tweet collection name
+tweet_collection_name = 'tweets'
+
+# follwers id collection name
+followerID_collection_name = 'followerID'
+
+# log collection name
+log_collection_name = 'log'
+
+# followers id:name mapping collection name
+id_name_collection_name = 'id_name'
+
+# number of tweets to colect for each follower
+tweet_num = 300
--- component_twitter_networks/get_followers_tweets_mongodb.py
+++ component_twitter_networks/get_followers_tweets_mongodb.py
@@ -1,32 +1,51 @@
 import os
 import json
 import tweepy
+import datetime
 from get_tweepy_api import API
-from config import id_or_screen_name,project_name,db_name,collection_name
+from config import id_or_screen_name,project_name,db_host,db_port,db_name,followerID_collection_name,id_name_collection_name,tweet_collection_name,tweet_num,log_collection_name
 from pymongo import MongoClient
 
 PROJECT_DIRECTORY = 'output/project/' + project_name
 
-id_or_screen_name = id_or_screen_name.split(' ')
 
-Common_Followers = []
+# connect to localhost mongodb
+client = MongoClient(db_host, db_port)
 
-for name in id_or_screen_name:
-    DIRECTORY = 'output/' + name
-    FOLLOWERS_FILE_NAME = DIRECTORY + '/followers.json'
+# set the database name
+db = client[db_name]
 
-    with open(FOLLOWERS_FILE_NAME, encoding='utf-8') as file:
-        FOLLOWERS_USER_IDS = json.load(file)
+uniq_followers = db[followerID_collection_name].distinct("followerID")
 
-    Common_Followers.append(FOLLOWERS_USER_IDS)
-
-flat_list = [item for sublist in Common_Followers for item in sublist]
-
-Common_Followers = set(flat_list)
-Common_Followers = list(Common_Followers)
+uniq_len = len(uniq_followers)
 
 LOG_FILE_NAME = PROJECT_DIRECTORY + '/followers_tweets_mongodb_log.json'
 
+
+# followers already collected
+result = db[id_name_collection_name].find({},{"_id":0,"id":1})
+done_followers = []
+for doc in result:
+    done_followers.append(doc["id"])
+
+#for i in done_followers:
+#    print("- ", i)
+
+# update the uniq_followers list
+uniq_followers = [x for x in list(uniq_followers) if x not in done_followers]
+
+#for i in uniq_followers:
+#    print(i)
+
+
+# if this is the continue collection recovered from the previous collection process
+if len(uniq_followers) != uniq_len:
+    # delete the last id name mapping and will re-collect from this one 
+    result = db[id_name_collection_name].delete_one({"id":done_followers[-1]})
+    # delete the last one's tweets and will re-collect from this one
+    result = db[tweet_collection_name].delete_many({"user.id":done_followers[-1]})
+    # insert the last one into the begining of the new uniq_followers list
+    uniq_followers.insert(0, done_followers[-1])
 
 # iterate over followers
 # get 100 latest tweets from each of those
@@ -34,41 +53,41 @@ LOG_FILE_NAME = PROJECT_DIRECTORY + '/followers_tweets_mongodb_log.json'
 TWEETS = {}
 LOG = []
 
-# connect to localhost mongodb
-client = MongoClient('localhost', 27017)
 
-# set the database name
-#db = client.TwitterFollowersTUT
-#db = client.testTweets
-db = client[db_name]
+id_or_screen_name = id_or_screen_name.split(' ')
 
-for follower_id in Common_Followers:
-    TWEETS[follower_id] = []
-    cursor = tweepy.Cursor(API.user_timeline, user_id=follower_id, count=500).items(500)
+for follower_id in uniq_followers:
+    cursor = tweepy.Cursor(API.user_timeline, user_id=follower_id, count=tweet_num).items(tweet_num)
+    # build the id:name map at the same time
+    k = 0
     try:
         for item in cursor:
-            #TWEETS[follower_id].append(item._json)
-            # insert into the collection tweets
-            #result = db.tweets.insert_one(item._json)
-            result = db[collection_name].insert_one(item._json)
-
+            result = db[tweet_collection_name].insert_one(item._json)
+            if k == 0:
+                result = db[id_name_collection_name].insert_one({"id":item._json['user']['id'],"name":item._json['user']['name'],"screen_name":item._json['user']['screen_name']})
+                k = 1
+            
     except tweepy.TweepError as error:
         ERROR_STRING = 'Tweepy error: {}. Failed to retrieve tweets of follower {}'.format(error.reason, follower_id)
-        print(ERROR_STRING)
+        #print(ERROR_STRING)
         # LOG[follower_id] = ERROR_STRING
         LOG.append(ERROR_STRING)
+        result = db[log_collection_name].insert_one({"time":datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"), "msg":ERROR_STRING})
         continue
     except StopIteration:
-        print("StopIteration")
+        #print("StopIteration")
+        EXP_STRING = 'StopIteration'
+        LOG.append(EXP_STRING)
+        result = db[log_collection_name].insert_one({"time":datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"), "msg":EXP_STRING})
         break
     except:
-        print("Unkown error")
+        #print("Unkown error")
+        UNKNOWNERR_STRING = 'Unknown error while processing follower {}'.format(follower_id)
         LOG.append('Unknown error while processing follower {}'.format(follower_id))
+        result = db[log_collection_name].insert_one({"time":datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"), "msg":UNKNOWNERR_STRING})
+        continue
 
-#with open(TWEETS_FILE_NAME, 'w') as f:
-#    json.dump(TWEETS, f, indent=4)
-
-print("save the log")
+#print("save the log")
 if not os.path.exists(PROJECT_DIRECTORY):
     os.makedirs(PROJECT_DIRECTORY)
 with open(LOG_FILE_NAME, 'w') as f:
